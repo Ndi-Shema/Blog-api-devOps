07c79f48e46ade267060fa1c5fe22e44e5e33c66
Ndi-Shema <f.shema@alustudent.com>
2024-07-30 21:53:36 +0200

    Eslint error correction

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,9 @@
 // eslint.config.js
-module.exports = {
-  languageOptions: {
-    parserOptions: {
-      ecmaVersion: 2021,
-    },
-  },
-  plugins: [
-    // List your plugins here
-  ],
+const { compatConfig } = require('eslint-config-compat');
+
+module.exports = compatConfig({
+  extends: 'your-shareable-config',
   rules: {
-    'indent': ['error', 2],
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'always'],
-    'no-unused-vars': ['error', { 'args': 'none' }],
-    'no-console': 'warn',
-    'linebreak-style': ['error', 'unix'],
-    'camelcase': ['error', { properties: 'always' }],
-    'strict': ['error', 'global'],
-    'no-magic-numbers': ['warn', { 'ignore': [0, 1] }],
-    'consistent-return': 'error',
+    // Your rules here
   },
-};
+});
