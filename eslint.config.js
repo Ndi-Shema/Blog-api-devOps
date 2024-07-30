e1c96a7d595edbc8392a30090739d55baee820bd
Ndi-Shema <f.shema@alustudent.com>
2024-07-30 22:01:52 +0200

    Eslint error correction

--- eslint.config.js
+++ eslint.config.js
@@ -14,16 +14,7 @@ module.exports = [
       // 'plugin-name': require('eslint-plugin-plugin-name')
     },
     rules: {
-      'indent': ['error', 2],
-      'quotes': ['error', 'single'],
-      'semi': ['error', 'always'],
-      'no-unused-vars': ['error', { 'args': 'none' }],
-      'no-console': 'warn',
-      'linebreak-style': ['error', 'unix'],
-      'camelcase': ['error', { properties: 'always' }],
-      'strict': ['error', 'global'],
-      'no-magic-numbers': ['warn', { 'ignore': [0, 1] }],
-      'consistent-return': 'error',
+      
     },
   }
 ];
