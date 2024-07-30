30a3310887d940ceb5c01b8498db6484e57100b4
Ndi-Shema <f.shema@alustudent.com>
2024-07-30 17:13:14 +0200

    intergrating winston in app

--- src/app.js
+++ src/app.js
@@ -7,6 +7,11 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
+const logger = require('./logger');
+
+// Example usage:
+logger.info('Server is starting...');
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use('/api/users', userRoutes);
